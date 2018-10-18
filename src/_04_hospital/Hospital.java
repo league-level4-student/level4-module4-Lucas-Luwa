ebdc6f364d378573255ed4fb50f21d3749788d5a
Lucas-Luwa <league@WTS21.attlocal.net>
2018-10-17 18:27:14 -0700

    hospital ip
    ipppppp

--- src/_04_hospital/Hospital.java
+++ src/_04_hospital/Hospital.java
@@ -1,5 +1,16 @@
 package _04_hospital;
 
-public class Hospital {
+public class Hospital extends Doctor{
+	Doctor 
+	public static void main(String[] args) {
 
+	}
+
+	public static void addDoctor() {
+
+		
+	}
+	public static void getDoctors() {
+		
+	}
 }
